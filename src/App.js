2176cdfdd18f0c47cd736b2aa849f1f85c0ca7e2
DESKTOP-FSGA4UF\erich <13458969+erich564@users.noreply.github.com>
2022-03-28 17:15:01 -0700

    Added cards and margin

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ import InputLabel from '@mui/material/InputLabel';
 import DateAdapter from '@mui/lab/AdapterMoment';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 
 function App() {
   const [age, setAge] = React.useState('');
@@ -21,79 +25,76 @@ function App() {
     setAge(event.target.value);
   };
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <LocalizationProvider dateAdapter={DateAdapter}>
-      <Container>
-        <br />
-        <CssBaseline />
-        Loan origination date
-        
-        <DatePicker
-          label="Basic example"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-        <br />
-        <FormControl sx={{  minWidth: 150 }}>
-          <InputLabel>Type</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
-            defaultValue={2}
-          >
-            <MenuItem value={1}>15-year fixed</MenuItem>
-            <MenuItem value={2}>30-year fixed</MenuItem>
-            <MenuItem value={3}>10/1 ARM</MenuItem>
-            <MenuItem value={4}>7/1 ARM</MenuItem>
-            <MenuItem value={5}>5/1 ARM</MenuItem>
-            <MenuItem value={6}>3/1 ARM</MenuItem>
-          </Select>
-        </FormControl>
-        <br />
-        <TextField
-          margin="normal"
-          required
-          id="interest"
-          label="Interest Rate"
-          name="interest"
-          autoComplete="interest"
-        />
-        <br />
-        <TextField
-          margin="normal"
-          required
-          id="term"
-          label="Term"
-          name="term"
-          autoComplete="term"
-        />
-        <br /><br />
-        <Button variant="contained">Compare</Button>
-      </Container>
+      
+        <Stack spacing={2} style={{margin: 15}}>
+          <Item>
+          <br />
+          <CssBaseline />
+          Loan origination date
+          
+          <DatePicker
+            label="Basic example"
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <br />
+          <FormControl sx={{  minWidth: 150 }}>
+            <InputLabel>Type</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+              defaultValue={2}
+            >
+              <MenuItem value={1}>15-year fixed</MenuItem>
+              <MenuItem value={2}>30-year fixed</MenuItem>
+              <MenuItem value={3}>10/1 ARM</MenuItem>
+              <MenuItem value={4}>7/1 ARM</MenuItem>
+              <MenuItem value={5}>5/1 ARM</MenuItem>
+              <MenuItem value={6}>3/1 ARM</MenuItem>
+            </Select>
+          </FormControl>
+          <br />
+          <TextField
+            margin="normal"
+            required
+            id="interest"
+            label="Interest Rate"
+            name="interest"
+            autoComplete="interest"
+          />
+          <br />
+          <TextField
+            margin="normal"
+            required
+            id="term"
+            label="Term"
+            name="term"
+            autoComplete="term"
+          />
+          <br /><br />
+          <Button variant="contained">Compare</Button>
+          </Item>
+          <Item>
+            abc123
+          </Item>
+        </Stack>
     </LocalizationProvider>
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained">Hello World</Button>
-      </header>
-    </div>*/
   );
 }
 
