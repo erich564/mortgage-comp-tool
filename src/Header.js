830af6ec3bfbc74e0d058a84bbe7190c018d7ee6
DESKTOP-FSGA4UF\erich <13458969+erich564@users.noreply.github.com>
2022-04-19 16:13:59 -0700

    In Header replaced Stack with ButtonGroup

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ShareIcon from '@mui/icons-material/Share';
-import { Button, Divider, Stack } from '@mui/material';
+import { Button, ButtonGroup, Menu, MenuItem } from '@mui/material';
 import { useState } from 'react';
 import InfoDialog from './InfoDialog';
 import ShareDialog from './ShareDialog';
@@ -7,26 +8,47 @@ import ShareDialog from './ShareDialog';
 export default function Header({ state, handleSampleData }) {
   const [isInfoDialogOpen, setIsInfoDialogOpen] = useState(false);
   const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
+  const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+  const handleMenuClose = () => setMenuAnchorEl(null);
+
+  const selectMenuItem = n => {
+    handleSampleData(n);
+    handleMenuClose();
+  };
+  const buttonStyle = { px: 2 };
 
   return (
     <>
-      <Stack
-        direction="row"
-        divider={<Divider orientation="vertical" flexItem />}
-        spacing={1.25}
-        justifyContent="center"
-      >
-        <Button onClick={() => setIsInfoDialogOpen(true)}>Information</Button>
-        <Button onClick={() => handleSampleData(0)}>Sample Data 1</Button>
-        <Button onClick={() => handleSampleData(1)}>Sample Data 2</Button>
-        <Button onClick={() => handleSampleData(2)}>Sample Data 3</Button>
+      <ButtonGroup variant="text" sx={{ justifyContent: 'center' }}>
+        <Button onClick={() => setIsInfoDialogOpen(true)} sx={buttonStyle}>
+          Information
+        </Button>
+        <Button
+          onClick={e => setMenuAnchorEl(e.currentTarget)}
+          endIcon={<KeyboardArrowDownIcon />}
+          sx={buttonStyle}
+        >
+          Examples
+        </Button>
         <Button
           onClick={() => setIsShareDialogOpen(true)}
           startIcon={<ShareIcon />}
+          sx={buttonStyle}
         >
           Share
         </Button>
-      </Stack>
+      </ButtonGroup>
+
+      <Menu
+        keepMounted
+        anchorEl={menuAnchorEl}
+        open={Boolean(menuAnchorEl)}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={() => selectMenuItem(0)}>Sample Data 1</MenuItem>
+        <MenuItem onClick={() => selectMenuItem(1)}>Sample Data 2</MenuItem>
+        <MenuItem onClick={() => selectMenuItem(2)}>Sample Data 3</MenuItem>
+      </Menu>
 
       <InfoDialog
         isInfoDialogOpen={isInfoDialogOpen}
