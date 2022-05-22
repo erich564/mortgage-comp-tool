73087172b82a90c3d8a4bd4b8da3717788818f1f
DESKTOP-FSGA4UF\erich <13458969+erich564@users.noreply.github.com>
2022-05-21 18:13:57 -0700

    Disable react dev tools in production

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import { CssBaseline } from '@mui/material';
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import disableReactDevTools from './disableReactDevTools';
 import './index.css';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 render(
   <StrictMode>
