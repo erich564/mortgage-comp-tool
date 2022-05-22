88a19f59f5bc85fb8178414ff74fc43889b83822
DESKTOP-FSGA4UF\erich <13458969+erich564@users.noreply.github.com>
2022-05-21 18:27:11 -0700

    Removed disableReactDevTools.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import { CssBaseline } from '@mui/material';
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import disableReactDevTools from './disableReactDevTools';
 import './index.css';
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 render(
   <StrictMode>
